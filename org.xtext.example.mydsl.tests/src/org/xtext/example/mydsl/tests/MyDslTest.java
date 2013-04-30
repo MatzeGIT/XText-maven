8531ac8931d7502ddba7696734e9b262544e108d
plauschin <Mathias.Plauschin@gmx.de>
2013-04-30 12:16:31 +0200

    XText-Maven Source V0.0

---
+++ org.xtext.example.mydsl.tests/src/org/xtext/example/mydsl/tests/MyDslTest.java
@@ -0,0 +1,11 @@
+package org.xtext.example.mydsl.tests;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MyDslTest {
+
+    @Test
+    public void test() {
+            assertTrue(true);
+    }
+}
